7d3fec1904929c82d607d1857b1f2222ac56690b
CPX <chinnathorn.p@gmail.com>
2022-07-09 14:00:39 +0700

    Update Local branch

--- src/fetch.js
+++ src/fetch.js
@@ -2,15 +2,33 @@ var ul = '';
 var added = false;
 var reg = 'Loading';
 
-ul = "https://apiweb.cpxdev.tk";
-reg = "Worldwide"
-
+fetch('https://ipapi.co/json/')
+  .then(response => response.json())
+  .then(data => {
+    switch(data.continent_code) {
+      case "AS": case "OC": {
+        ul = 'https://cpxapiweb2.azurewebsites.net'
+        reg = 'AS'
+        break;
+      }
+      case "AF": case "EU": {
+        ul = 'https://cpxapiweb.azurewebsites.net'
+        reg = 'EU'
+        break;
+      }
+      default: {
+        ul = 'https://apiweb.cpxdev.tk'
+        reg = 'US'
+        break;
+      }
+    }
+});
 
 function Flowup() {
-    return {
-        ul: ul,
-        nme: reg
-    }
+      return {
+             ul: ul,
+             nme: reg
+         }
 }
 
 export default Flowup;
